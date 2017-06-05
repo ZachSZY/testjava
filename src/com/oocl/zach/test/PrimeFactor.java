556954d362a5e929020a6c6ffda5ed29cd2fde3f
Zach Sha <zach.sha@oocl.com>
2017-06-05 18:45:00 +0800

    prime factor

--- src/com/oocl/zach/test/PrimeFactor.java
+++ src/com/oocl/zach/test/PrimeFactor.java
@@ -1,10 +1,28 @@
 package com.oocl.zach.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by SHAZA on 6/5/2017.
  */
 public class PrimeFactor {
-    public static String primeFactor(int num){
-        return null;
+    public static List primeFactor(int num) {
+        List result = new ArrayList();
+//        result.add(2);
+        if (num < 2) {
+            return null;
+        } else {
+            int i = 2;
+            while (i <= num) {
+                if (num % i == 0) {
+                    result.add(i);
+                    num = num / i;
+                } else {
+                    i++;
+                }
+            }
+        }
+        return result;
     }
 }
--- test/com/oocl/zach/test/PrimeFactorTest.java
+++ test/com/oocl/zach/test/PrimeFactorTest.java
@@ -1,5 +1,9 @@
 package com.oocl.zach.test;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 /**
@@ -7,4 +11,13 @@ import static org.junit.Assert.*;
  */
 public class PrimeFactorTest {
 
+    @Test
+    public void GIVEN_a_num_WHEN_prime_factor_THEN_compare_result(){
+        assertEquals(Arrays.asList(2),PrimeFactor.primeFactor(2));
+        assertEquals(Arrays.asList(3),PrimeFactor.primeFactor(3));
+        assertEquals(Arrays.asList(2,2),PrimeFactor.primeFactor(4));
+        assertEquals(Arrays.asList(3,3),PrimeFactor.primeFactor(9));
+        assertEquals(Arrays.asList(2,2,3),PrimeFactor.primeFactor(12));
+
+    }
 }
