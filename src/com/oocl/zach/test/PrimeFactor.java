f6f56528818da237e0ef8305ae4afbf0b8c295d3
Zach Sha <zach.sha@oocl.com>
2017-06-05 17:41:48 +0800

    first commint

---
+++ src/com/oocl/zach/test/PrimeFactor.java
@@ -0,0 +1,10 @@
+package com.oocl.zach.test;
+
+/**
+ * Created by SHAZA on 6/5/2017.
+ */
+public class PrimeFactor {
+    public static String primeFactor(int num){
+        return null;
+    }
+}
---
+++ test/com/oocl/zach/test/PrimeFactorTest.java
@@ -0,0 +1,10 @@
+package com.oocl.zach.test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by SHAZA on 6/5/2017.
+ */
+public class PrimeFactorTest {
+
+}
